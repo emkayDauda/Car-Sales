749b17f19271cef415dda6cdf49e39bd01f0529a
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-09 13:04:29 +0100

    :sparkles: Set up simple carReducer and actionTypes

---
+++ src/state/reducers.js
@@ -0,0 +1,21 @@
+import * as types from './actionTypes'
+
+const initialCar = {
+    price: 26395,
+    name: '2019 Ford Mustang',
+    image:
+      'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+    features: []
+  },
+export const carReducer = (car = initialCar, action) => {
+    switch(action.type){
+        case types.ADD_FEATURE: {
+            return {
+                ...car,
+                features: [...car.features, action.payload.feature]
+            }
+        };
+
+        default: return state
+    }
+}
